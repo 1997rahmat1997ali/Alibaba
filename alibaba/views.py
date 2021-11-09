d17d013a41664727e1131c64bf1a2d0f13a31944
Anish-kumar-ghub <ansh985nra@gmail.com>
2021-11-09 09:30:02 +0530

    login part done

--- alibaba/views.py
+++ alibaba/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from alibaba.forms import UserForm
 from django.contrib import messages
 from django import forms
-
+from alibaba.models import userinfo
 # Create your views here.
 
 def new(r):
@@ -38,3 +38,24 @@ def sig(r):
             context["mismatch"] = 1
             context["message"] = 'provide correct email'
             return render(r,'signupin.html',context)
+def log_in(r):
+    x=0
+    u=userinfo.objects.all()
+    if(r.method=='POST'):
+        email=r.POST['email']
+        pd=r.POST['pd']
+        for i in u:
+            if i.email==email:
+                if i.pd==pd:
+                    x=1
+                    break
+        if(x==1):
+            return HttpResponse('welcome')
+        else:
+            context = {}
+            context["mismatch"] = 1
+            context["message"] = 'your email or password is incorrect'
+            return render(r,'signupin.html',context)
+    
+    
+    
